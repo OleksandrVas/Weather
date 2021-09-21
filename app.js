1bac7e9b4c0de98a7a2e9626f92567e69eb755a8
Oleksandr <sasha228123@gmail.com>
2021-09-21 22:36:43 +0300

    created weather menu

---
+++ app.js
@@ -0,0 +1,56 @@
+// function showWeather() {
+//     let cityNameOfWeatherMap = prompt("Введите город , погоду которого вы хотите узнать")
+    
+    let data = fetch(`http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+    let nameOfPalce = prompt("Введите город , погоду которого вы хотите узнать");
+
+
+class Weather {
+    constructor ( data , parent  ) {
+                this.place = nameOfPalce
+                this.temp = data.main.temp;
+                this.pressure = data.main.pressure ;
+                this.humidity = data.main.humidity ;
+                this.speed = data.wind.speed;
+                this.deg = data.wind.deg;
+                this.main = data.weather[0].main
+                this.icon = data.weather[0].icon;
+                this.parent = document.querySelector(".article")
+            }
+    render() {
+        let article = document.createElement("article-box");
+        article.classList.add("col-6");
+        article.innerHTML = `
+        <div class="article-box">
+                    <div class="article-place">Weather for <span class="weather-place">${this.place}</span></div>
+                    <hr>
+                    <div class="article-temp">Tempereture :${Math.round(this.temp)}&degC</div>
+                    <div class="article-pres">Pressure :  ${this.pressure} hPa</div>
+                    <div class="article-hum">Humitity ${this.humidity}%</div>
+                    <div class="article-sp">Speed : ${this.speed} m/s</div>
+                    <div class="article-deg">Deg. : ${this.deg}&deg</div>
+                    <div class="article-main">Sky : <span>${this.main}</span></div>
+                    <div class="article-icon"><img src="https://openweathermap.org/img/wn/${this.icon}@2x.png"></img></div>
+                </div>
+        `;
+        this.parent.append(article)
+    }
+    // https://openweathermap.org/img/wn/02d@2x.png
+}
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${nameOfPalce}&units=metric&APPID=5d066958a60d315387d9492393935c19`)
+.then((response) => response.json())
+    .then((data) => new Weather(data, parent).render());
+
+
+
+    // fetch("http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19")
+    // .then((response) => response.json())
+    // .then(data => {document.write(data.main.temp);
+    //                 document.write(data.main.pressure)
+    //                 document.write(data.main.humidity)
+    //                 document.write(data.wind.speed)
+    //                 document.write(data.wind.deg)
+
